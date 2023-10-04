a927e76ad6776607276dc466463d5db03dc80289
jose-tl <j.tonnesenlucas@gmail.com>
2023-10-04 11:11:43 +0200

    Ex2 done

--- ex2.js
+++ ex2.js
@@ -1,23 +1,21 @@
 function rollTheDice(num){
-    // Initialize array
-    let numberOfSixes = 0;
 
     for (let i = 0; i < num; i++) {
         //Random number between 1 and 6:
         let diceRoll = Math.ceil(Math.random()*6);
         // Check if the dice roll is 6
         if (diceRoll === 6){
-            numberOfSixes++;
             console.log("You just hit 6!")
         }
     }
 }
 
+// Better function, added stuff
 function rollTheDiceAlt(num) {
     // init dice rolls
     const diceRolls = [];
 
-    // populate dice rolls with numbers
+    // populate dice rolls with random numbers
     for (let i = 0; i < num; i++) {
         diceRolls[i] = Math.ceil(Math.random()*6);
 
@@ -25,18 +23,19 @@ function rollTheDiceAlt(num) {
 
     // Check if diceRolls contains only 6
     const onlySixes = diceRolls.every((e) => e === 6);
-    if (onlySixes) return "Jackpot";
+    if (onlySixes) return console.log("Jackpot");
 
+    // Log out, every time array contains 6
     for (const e in diceRolls) {
-        if (e === 6) {
+        if (diceRolls[e] === 6) {
             console.log("You just hit 6")
         }
     }
 
 }
 
-rollTheDiceAlt(3);
-/*
+rollTheDiceAlt(10);
+
 // This means roll the dice 3 times. 6 is hit once
 rollTheDice(3);
 // You just hit 6
@@ -47,4 +46,4 @@ rollTheDice(5);
 // You just hit 6
 
 // Here the user rolls 5 times but does not hit 6 even once
-rollTheDice(5);*/
+rollTheDice(5);
