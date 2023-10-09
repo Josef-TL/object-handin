182752871c469546cf99c4cd93f3b32e1202d7fc
jose-tl <j.tonnesenlucas@gmail.com>
2023-10-09 12:42:53 +0200

    Ex3 done

--- ex3.js
+++ ex3.js
@@ -1,16 +1,16 @@
-const positiveWordsList = ["happy", "awesome", "super"];
-const negativeWordsList = ["bad"]
+const positiveWordsList = ["happy", "awesome", "super", "love"];
+const negativeWordsList = ["bad", "angry", "hate", "dead"]
 
 // Filter function
 function getFilteredString(stringToBeFiltered, matchThisList){
     let result = [];
 
     for (const i in matchThisList) {
-        // Pushing filtered array to positiveWords
-        result = stringToBeFiltered.filter(
-            // Filters thorugh my predefined words i see as positive, each word that match gets pushed
+        // Concatenating filtered array to result.
+        result = result.concat( stringToBeFiltered.filter(
+            // Filters thorugh my predefined words i see as positive or negative, each word that match gets pushed
             (el) => el.includes(matchThisList[i])
-        );
+        ));
     }
     return result
 }
@@ -18,36 +18,40 @@ function getFilteredString(stringToBeFiltered, matchThisList){
 
 
 function getSentimentScore(str){
+    // If str is not string, dont run
+    if(typeof str !== "string") return "Please input a string";
+
+    // make an array, else the filter function wont work.
     let splitString = [""];
 
-    // split the string, i get an array
+    // split the string
     // check if str has spaces
     if (str.includes(" ")){
         splitString = str.split(" ")
-    } else {splitString = str}
+    } else {splitString.push(str)}
 
 
-    // Filter the split array against a "positive" word list
-    const positiveWords = getFilteredString(splitString,positiveWordsList);
+    // Filter the split array against a "positive" and "negative" word list.
+    const positiveWords = getFilteredString(splitString, positiveWordsList);
+    const negativeWords = getFilteredString(splitString, negativeWordsList);
 
-
-
-    console.log(positiveWords);
-
-    // Filter the split array against a "negative" word list
     // score is calculated; length of positive array minus length of negative
-    // values is pushed into an object
+    const wordScore = positiveWords.length - negativeWords.length;
+
+    // values is returned as an object
+    return {
+        score: wordScore,
+        positiveWords: positiveWords,
+        negativeWords: negativeWords
+    };
 
 }
 
+// call the function and log it
+// The functions counts words, even with no spaces. It cannot print the correct words, but gives a score.
+console.log(getSentimentScore('Dead inside.'));
+console.log(getSentimentScore('I am mega super awesome happy'));
+console.log(getSentimentScore('I am mega superawesomehappy'));
 
-const sentimentScoreObject = getSentimentScore('I am mega super awesome happy happy');
 
-console.log(sentimentScoreObject);
-/*
-{
-  score: 3,
-  positiveWords: ['happy', 'awesome', 'super'],
-  negativeWords: [],
-}
-*/
+
